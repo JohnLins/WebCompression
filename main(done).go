d12422e619fe6b5ab1644cad97296bed5a1262da
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-11-19 18:09:05 -0800

    Update main(done).go

--- main(done).go
+++ main(done).go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"fmt"
+  "net/http"
+  "io/ioutil"
 )
 
-func remove(s []byte, i int) []byte {
-    return append(s[:i], s[i+1:]...)
+func compressHTML(url string) ([]byte, int) {
+	var reduction int = 0; 
+	remove := func(s []byte, i int) []byte {
+		return append(s[:i], s[i+1:]...)
+	}
+
+	html := getHTML(url)
+	for i := 0; i<len(html)-1; i++ {
+
+    //return
+    if html[i] == 13 {
+      html = remove(html, i)
+			reduction++;
+    }
+
+    //Space
+		for html[i + 1] == 32 && html[i] == 32 {
+			html = remove(html, i)
+			reduction++;
+		}
+    
+
+		}
+		
+		return html, reduction
 }
 
 func main() {
-html := []byte("<html><div>ddq eewat  twt<span>  </span><div>          </html>")
 
-for i := 0; i<len(html)-1; i++ {
-for html[i + 1] == 32 && html[i] == 32 {
-	fmt.Println("Space")
+  var compressedHTML, reduction = compressHTML("http://linsintegrations.co")
 	
-	html = remove(html, i)
-}
-fmt.Println(string(html[i]))
+	fmt.Println(string(compressedHTML))
+
+  fmt.Println(reduction, "B")
+	 
 }
 
-fmt.Println(string(html))
-
+func getHTML(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return html
 }
